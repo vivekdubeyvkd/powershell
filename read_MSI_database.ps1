3372dcef65fcc5eb8bf6d46412b8799c306047f2
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2019-06-27 09:59:53 +0530

    Create read_MSI_database.ps1

---
+++ read_MSI_database.ps1
@@ -0,0 +1,63 @@
+[CmdletBinding()] 
+param(
+	[Parameter(Mandatory=$True, 
+	ValueFromPipeline=$True, 
+	ValueFromPipelineByPropertyName=$True, 
+	HelpMessage='Error !!!!! Invalid MSI path !!!!! please provide a valid MSI you would like to query and rerun ...... exiting .......')]
+	[IO.FileInfo[]] $MSIFilePath
+)
+
+function Get-MsiDatabaseVersion {
+	    param (
+		     [IO.FileInfo[]] $FilePath
+	    )
+
+	    try {
+		     $com_object = New-Object -com WindowsInstaller.Installer 
+
+         $database = $com_object.GetType().InvokeMember( 
+            "OpenDatabase", 
+            "InvokeMethod", 
+            $Null, 
+            $com_object, 
+            @($FilePath.FullName, 0) 
+        ) 
+        $query = "SELECT * FROM Registry" 
+        $View = $database.GetType().InvokeMember( 
+            "OpenView", 
+            "InvokeMethod", 
+            $Null, 
+            $database, 
+            ($query) 
+        )
+        $View.GetType().InvokeMember("Execute", "InvokeMethod", $Null, $View, $Null) 
+ 
+        $record = $View.GetType().InvokeMember( 
+            "Fetch", 
+            "InvokeMethod", 
+            $Null, 
+            $View, 
+            $Null 
+        )
+        $msi_props = @{} 
+		    $count = 0
+        while ($record -ne $null) { 
+            $msi_props[$record.GetType().InvokeMember("StringData", "GetProperty", $Null, $record, 1)] = $record.GetType().InvokeMember("StringData", "GetProperty", $Null, $record, 6) 
+            $record = $View.GetType().InvokeMember( 
+                "Fetch", 
+                "InvokeMethod", 
+                $Null, 
+                $View, 
+                $Null 
+            ) 
+			      $count++
+         }
+         $count
+         $msi_props
+
+	} catch {
+		throw "Failed to get MSI file version the error was: {0}." -f $_
+	}
+}
+
+Get-MsiDatabaseVersion -FilePath $MSIFilePath
