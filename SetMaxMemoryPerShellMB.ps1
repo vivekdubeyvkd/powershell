bdc60f734044f77907db0b7f0ac04a448c8790c9
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2020-02-19 21:33:06 +0530

    Update SetMaxMemoryPerShellMB.ps1

--- SetMaxMemoryPerShellMB.ps1
+++ SetMaxMemoryPerShellMB.ps1
@@ -1,4 +1,3 @@
-Set-Item WSMan:\localhost\Shell\MaxMemoryPerShellMB 5000
-Set-Item WSMan:\localhost\Plugin\Microsoft.PowerShell\Quotas\MaxMemoryPerShellMB 5000
-
+Set-Item WSMan:\localhost\Shell\MaxMemoryPerShellMB 30000 #30GB
+Set-Item WSMan:\localhost\Plugin\Microsoft.PowerShell\Quotas\MaxMemoryPerShellMB 30000 #30GB
 Restart-Service winrm
